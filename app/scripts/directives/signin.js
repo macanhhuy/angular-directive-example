77b72b050d06e7ae4ca9deab8a1688a3981d010a
Stuart Plumbley <stuplum@googlemail.com>
2013-01-18 09:18:11 +0000

    Update app/scripts/directives/signin.js

--- app/scripts/directives/signin.js
+++ app/scripts/directives/signin.js
@@ -6,8 +6,8 @@ angular.module('directive.signin', ['services.authentication']).
 
         return {
             restrict: 'E',
-            templateUrl: 'views/directive/signin.html',
-//            template: '<section class="sign-in" ng-show="show==true"><form class="well form-inline"><input type="email" placeholder="Username" ng-model="username" required /><input type="password" placeholder="Password" ng-model="password" required /><button ng-click="signin()">Signin</button></form><button ng-click="signout()">Signout</button></section>',
+            templateUrl: 'views/directive/signin.html', // using this method causes the tests to fail
+//            template: '<section class="sign-in" ng-show="show==true"><form class="well form-inline"><input type="email" placeholder="Username" ng-model="username" required /><input type="password" placeholder="Password" ng-model="password" required /><button ng-click="signin()">Signin</button></form><button ng-click="signout()">Signout</button></section>', // using this method causes the tests to pass
             replace: true,
             scope: true,
             link: function (scope, element, attrs) {
